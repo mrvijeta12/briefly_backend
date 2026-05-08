b4be1762dcce83b0073acfb2f3817e46f9ff0a1d
vijeta <vijetavarma1@gmail.com>
2026-05-08 16:52:27 +0530

    cors updated

--- server.js
+++ server.js
@@ -7,8 +7,29 @@ import cors from "cors";
 
 const app = express();
 const PORT = 5000;
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://briefly-backend-blond.vercel.app/",
+];
 
-app.use(cors());
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      // console.error(` CORS Blocked: ${origin} is not in allowed list`);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options(/(.*)/, cors(corsOptions)); // Fix for Express 5 crash
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
